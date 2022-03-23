c9ac74f6bbd58f88666649191213480c44c23d2e
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-03-23 15:58:01 -0300

    Fix app ppath

--- app/src/app.py
+++ app/src/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from src.schemas import Rentability
 import logging
 from src.database import fetch_price_history, fetch_price, mongo_import
-from src.utils import calculate_rentability, calculate_profit, process_csv
+from src.utils import calculate_markup, calculate_profit, process_csv
 from typing import Optional
 from fastapi_utils.tasks import repeat_every
 
@@ -28,7 +28,7 @@ async def get_bitcoin_price(_limit: int = 30, _skip: Optional[int] = 1):
 @app.get("/funds/bitcoin/rentability", response_model=Rentability)
 async def get_bitcoin_rentability(init_date:str, end_date:str, invest_value: float):
     in_dt, en_dt = await fetch_price(init_date), await fetch_price(end_date)
-    r = calculate_rentability(in_dt,en_dt)
+    r = calculate_markup(in_dt,en_dt)
     profit = calculate_profit(invest_value,r)
     response = {"rentability": r,
                 "profit_value": profit}
