910e4e2b2f20aec0aa36bded963de0d43f3b3b8f
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-03-26 16:45:13 -0300

    PEP formatting

--- app/src/database.py
+++ app/src/database.py
@@ -12,21 +12,23 @@ db = client["prices"]
 
 coll = db["btc_price"]
 
-async def fetch_price(date:str):
+
+async def fetch_price(date: str):
     document = await coll.find_one({"date": date})
     price = document["price_usd"]
     return price
 
+
 async def fetch_price_history(_limit: int, _skip: int) -> List:
     prices = []
-    cursor = coll.find({}).sort("date", -1 ).limit(_limit).skip(_skip)
+    cursor = coll.find({}).sort("date", -1).limit(_limit).skip(_skip)
     async for document in cursor:
         prices.append(Price(**document))
     return prices
 
-def mongo_import(_csv:dict) -> bool:
+
+def mongo_import(_csv: dict) -> bool:
     coll.create_index([('date', 1)], unique=True)
     coll.delete_many({})
     coll.insert_many(_csv)
     return True
-    
--- app/src/schemas.py
+++ app/src/schemas.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class Price(BaseModel):
     date: str
     price_usd: str
-    
+
+
 class Rentability(BaseModel):
     rentability: float
     profit_value: float
-    
