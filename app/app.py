6d5a90f9529696a0a04643c4b314d528a2d1bf29
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-03-26 22:17:24 -0300

    new docs endpoint

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ middleware = [
         allow_headers=['*']
     )
 ]
-app = FastAPI(middleware=middleware, openapi_prefix="/help")
+app = FastAPI(openapi_prefix="/help", middleware=middleware)
 
 
 @app.get('/')
