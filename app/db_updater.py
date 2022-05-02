8329b90c77cd70c2bf1a3f0a4995ce5fbae9a048
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-05-01 22:32:51 -0300

    Wrapper args for lambda

--- app/db_updater.py
+++ app/db_updater.py
@@ -1,14 +1,13 @@
 from app.database import mongo_import
 from app.utils import process_csv
 
-from mangum import Mangum
 
 def populate_db() -> None:
     _csv = process_csv(
         "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv")
+    print("Csv processed!")
     mongo_import(_csv)
     
-
-db_updater_handler = Mangum(populate_db)
-    
+populate_db()
+        
 # schedule.every().hour.do(populate_db)
