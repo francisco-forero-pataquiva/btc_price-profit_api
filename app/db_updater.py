d58625d71725c864b7ca84f21e257fbba23f6252
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-05-03 20:24:59 -0300

    Lambda Debugger

--- app/db_updater.py
+++ app/db_updater.py
@@ -5,8 +5,6 @@ from app.utils import process_csv
 def populate_db(event, lambda_context) -> None:
     _csv = process_csv(
         "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv")
-    print(MONGO_DETAILS)
     print("Csv processed!")
     mongo_import(_csv)
-populate_db(1,2) 
 # schedule.every().hour.do(populate_db)
--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class Price(BaseModel):
     date: str
     price_usd: str
+   
     
 class Rentability(BaseModel):
     rentability: float
