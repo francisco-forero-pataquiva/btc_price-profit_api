6ea35ebfeab256628dfe6856e39420e16d0e39d8
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-05-03 20:19:08 -0300

    Lambda Debugger

--- app/db_updater.py
+++ app/db_updater.py
@@ -1,11 +1,12 @@
-from app.database import mongo_import
+from app.database import MONGO_DETAILS, mongo_import
 from app.utils import process_csv
 
 
 def populate_db(event, lambda_context) -> None:
     _csv = process_csv(
         "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv")
+    print(MONGO_DETAILS)
     print("Csv processed!")
     mongo_import(_csv)
-            
+populate_db(1,2) 
 # schedule.every().hour.do(populate_db)
