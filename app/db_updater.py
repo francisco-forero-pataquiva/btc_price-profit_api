dfd04baf19c68905b7f5cbb2fced8a470372bdf2
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-05-04 12:42:14 -0300

    Cleaner db updater

--- app/db_updater.py
+++ app/db_updater.py
@@ -1,10 +1,8 @@
-from app.database import MONGO_DETAILS, mongo_import
+from app.database import mongo_import
 from app.utils import process_csv
 
 
 def populate_db(event, lambda_context) -> None:
-    _csv = process_csv(
-        "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv")
-    print("Csv processed!")
-    mongo_import(_csv)
-# schedule.every().hour.do(populate_db)
+    _csv = process_csv("https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv")
+    len_csv =  len(_csv)
+    mongo_import(len_csv,_csv)
