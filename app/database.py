e0735f715b1b4e2aec34ae3047d4eec317d67c80
francisco.pataquiva <franciscodepaulafopa@gmail.com>
2022-05-04 09:21:05 -0300

    Additional print statments

--- app/database.py
+++ app/database.py
@@ -25,8 +25,10 @@ async def fetch_price_history(_limit: int, _skip: int) -> List:
     return prices
 
 def mongo_import(_csv: dict) -> bool:
+    print(MONGO_DETAILS)
     coll.create_index([('date', 1)], unique=True)
     coll.delete_many({})
     coll.insert_many(_csv)
+    print(coll)
     return True
     
